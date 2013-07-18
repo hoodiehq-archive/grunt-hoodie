8c8080e0521eb1cac8e58e6936442ada90295c3a
Robert Katzki <katzki@ubilabs.net>
2013-07-18 15:48:57 +0200

    Start hoodie and trigger callback with the stack.json content

--- tasks/hoodie.js
+++ tasks/hoodie.js
@@ -13,38 +13,57 @@ module.exports = function(grunt) {
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
 
-  grunt.registerMultiTask('hoodie', 'Your task description goes here.', function() {
+  grunt.registerMultiTask('hoodie', 'Start hoodie and delay grunting till it is ready.', function() {
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
-      punctuation: '.',
-      separator: ', '
+        callback: function() {}
+      }),
+      fs = require('fs'),
+      path = require('path'),
+      exec = require('child_process').exec,
+      stackFile = path.resolve('data/stack.json'),
+      done = this.async(),
+      timerId = 0;
+
+    function setTicker(call, delay) {
+      clearTimeout(timerId);
+      timerId = setTimeout(call, delay);
+    }
+
+    function finish() {
+      var data = fs.readFileSync(stackFile, 'utf8'),
+        stack = JSON.parse(data);
+
+      options.callback(stack);
+      console.log('hoodie is ready!');
+      done();
+    }
+
+    function checkForStackFile() {
+      if (fs.existsSync(stackFile)) {
+        finish();
+        return;
+      }
+
+      setTicker(checkForStackFile, 100);
+    }
+
+    console.log('Starting hoodie.');
+
+    // Remove stack file
+    if (fs.existsSync(stackFile)) {
+      fs.unlinkSync(stackFile);
+    }
+
+    var hoodieProcess = exec('hoodie start', function(error, stdout, stderr) {
+      console.log('stdout: ' + stdout);
+      console.log('stderr: ' + stderr);
+      if (error !== null) {
+        console.log('exec error: ' + error);
+      }
     });
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
+    setTicker(checkForStackFile, 100);
   });
 
 };
